4f5359268999fbd6411be8dd7419d4bc735d08d6
Simeon Petkov <you@example.com>
2016-03-19 17:06:00 +0200

    Added ConsoleLogger with ILogger interface.

---
+++ Source/SQL-Db-Back-And-Restore/SQL-Db-Back-and-Restore.Logger/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace SQL_Db_Back_and_Restore.Logger
+{
+    using Contracts;
+    using System;
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            DateTime now = DateTime.Now;
+            string nowFormatted = now.ToString("dd/MM/yyyy HH:mm:ss:fff");
+            Console.WriteLine(string.Format("LOGGER {0}: {1}", nowFormatted, message));
+        }
+    }
+}
---
+++ Source/SQL-Db-Back-And-Restore/SQL-Db-Back-and-Restore.Logger/Contracts/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace SQL_Db_Back_and_Restore.Logger.Contracts
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
