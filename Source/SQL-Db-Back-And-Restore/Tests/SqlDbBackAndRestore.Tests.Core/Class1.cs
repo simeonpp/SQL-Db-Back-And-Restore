7817959260a0ca3602507cf7d59b40948a4e48e7
Simeon Petkov <you@example.com>
2016-03-20 10:10:44 +0200

    Introduced Test Library for Core library.

---
+++ Source/SQL-Db-Back-And-Restore/Tests/SqlDbBackAndRestore.Tests.Core/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace SqlDbBackAndRestore.Tests.Core
+{
+    using System;
+    using NUnit;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
